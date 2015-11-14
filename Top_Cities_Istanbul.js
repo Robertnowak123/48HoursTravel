8d4f7d28ff2f75350b8f883d8fcfdd77477f20b6
Robert Nowak <robert.andreas.nowak@gmail.com>
2015-11-14 20:45:29 +0100

    initial commit

---
+++ Top_Cities_Istanbul.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+	$("#hamburgermenu").click(function() {
+  		$(".slidemenu").toggle("fast");
+	});
+	
+	$("#hamburgermenu").hover(function(){
+		$(".hamburgermenu_li").css("background-color", "#B0B0B0");
+	}, function(){
+		$(".hamburgermenu_li").css("background-color", "white");
+
+	});
+});
+
